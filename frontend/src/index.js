260bc53a8204dba003b3193ccc732440b330b8ff
Gamzat Yusupov <gamzat.yusupov.97@mail.ru>
2023-09-29 13:57:40 +0300

    try #1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter, BrowserRouter as Router} from 'react-router-dom';
 import App from './components/App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
